527ff6b6599cfc20a7c0ced51a3921ae534a8fae
Brandon <devbrandoncraig@gmail.com>
2020-03-17 15:42:24 -0700

    Finished Sections 1 - 6

---
+++ Bar-Chart/js/main.js
@@ -0,0 +1,126 @@
+var margin = { left: 80, right: 20, top: 50, bottom: 100 };
+
+var width = 600 - margin.left - margin.right,
+  height = 400 - margin.top - margin.bottom;
+
+var flag = true;
+
+var t = d3.transition().duration(750);
+
+var g = d3.select("#chart-area")
+  .append("svg")
+  .attr("width", width + margin.left + margin.right)
+  .attr("height", height + margin.top + margin.bottom)
+  .append("g")
+  .attr("transform", "translate(" + margin.left + ", " + margin.top + ")");
+
+var xAxisGroup = g.append("g")
+  .attr("class", "x axis")
+  .attr("transform", "translate(0," + height + ")");
+
+var yAxisGroup = g.append("g").attr("class", "y axis");
+
+// X Scale
+var x = d3.scaleBand()
+  .range([0, width])
+  .padding(0.2);
+
+// Y SCALE
+var y = d3.scaleLinear().range([height, 0]);
+
+// X LABEL
+g.append("text")
+  .attr("y", height + 50)
+  .attr("x", width / 2)
+  .attr("font-size", "20px")
+  .attr("text-anchor", "middle")
+  .text("Month");
+
+// Y LABEL
+var yLabel = g.append("text")
+  .attr("y", -60)
+  .attr("x", -(height / 2))
+  .attr("font-size", "20px")
+  .attr("text-anchor", "middle")
+  .attr("transform", "rotate(-90)")
+  .text("Revenue");
+
+d3.json("data/revenues.json").then(function (data) {
+  // CLEAN DATA
+  data.forEach(function (d) {
+    d.revenue = +d.revenue;
+    d.profit = +d.profit;
+  });
+
+  d3.interval(function () {
+
+    var newData = flag ? data : data.slice(1);
+
+    update(newData);
+    flag = !flag;
+  }, 1000);
+
+  // RUN THE VIS FOR THE FIRST TIME
+  update(data);
+});
+
+function update(data) {
+
+  var value = flag ? "revenue" : "profit";
+
+  x.domain(data.map(function (d) { return d.month; }));
+
+  y.domain([0, d3.max(data, function (d) { return d[value]; })]);
+
+  // X AXIS
+  var xAxisCall = d3.axisBottom(x);
+
+  xAxisGroup.transition(t).call(xAxisCall);
+
+  // Y AXIS
+  var yAxisCall = d3.axisLeft(y).tickFormat(function (d) { return "$" + d; });
+
+  yAxisGroup.transition(t).call(yAxisCall);
+
+  // JOIN NEW DATA WITH OLD ELEMENTS.
+  var rects = g.selectAll("rect")
+    .data(data, function (d) {
+      return d.month;
+    });
+
+  // EXIT OLD ELEMENTS NOT PRESENT IN NEW DATA.
+  rects.exit()
+    .attr("fill", "red")
+    .transition(t)
+    .attr("y", y(0))
+    .attr("height", 0)
+    .remove();
+
+  // UPDATE OLD ELEMENTS PRESENT IN NEW DATA.
+  rects.transition(t)
+    .attr("y", function (d) { return y(d[value]); })
+    .attr("x", function (d) { return x(d.month); })
+    .attr("height", function (d) { return height - y(d[value]); })
+    .attr("width", x.bandwidth);
+
+  // ENTER NEW ELEMENTS PRESENT IN NEW DATA.
+  rects.enter()
+    .append("rect")
+    .attr("fill", "grey")
+    .attr("y", y(0))
+    .attr("height", 0)
+    .attr("x", function (d) { return x(d.month); })
+    .attr("width", x.bandwidth)
+
+    // AND UPDATE OLD ELEMENTS PRESENT IN NEW DATA.
+    .merge(rects)
+    .transition(t) // 750ms transition
+    .attr("x", function (d) { return x(d.month); })
+    .attr("width", x.bandwidth)
+    .attr("y", function (d) { return y(d[value]); })
+    .attr("height", function (d) { return height - y(d[value]); });
+
+  var label = flag ? "Revenue" : "Profit";
+
+  yLabel.text(label);
+}
--- Scatter-Plot/js/main.js
+++ Scatter-Plot/js/main.js
@@ -13,6 +13,23 @@ var g = d3.select("#chart-area")
 
 var time = 0;
 
+var interval;
+
+var formattedData;
+
+// TOOL-TIP
+var tip = d3.tip().attr('class', 'd3-tip')
+	.html(function (d) {
+		var text = "<strong>Country:</strong> <span style='color:crimson'>" + d.country + "</span><br>";
+		text += "<strong>Continent:</strong> <span style='color:crimson;text-transform:capitalize'>" + d.continent + "</span><br>";
+		text += "<strong>Life Expectancy:</strong> <span style='color:crimson'>" + d3.format(".2f")(d.life_exp) + "</span><br>";
+		text += "<strong>GDP Per Capita:</strong> <span style='color:crimson'>" + d3.format("$,.0f")(d.income) + "</span><br>";
+		text += "<strong>Population:</strong> <span style='color:crimson'>" + d3.format(",.0f")(d.population) + "</span><br>";
+		return text;
+	});
+g.call(tip);
+
+
 // SCALES
 var x = d3.scaleLog()
 	.base(10)
@@ -69,11 +86,36 @@ g.append("g")
 	.attr("class", "y axis")
 	.call(yAxisCall);
 
+var continents = ["europe", "asia", "americas", "africa"];
+
+var legend = g.append("g")
+	.attr("transform", "translate(" + (width - 10) +
+		"," + (height - 125) + ")");
+
+continents.forEach(function (continent, i) {
+	var legendRow = legend.append("g")
+		.attr("transform", "translate(0, " + (i * 20) + ")");
+
+	legendRow.append("rect")
+		.attr("width", 10)
+		.attr("height", 10)
+		.attr("fill", continentColor(continent));
+
+
+	legendRow.append("text")
+		.attr("x", -10)
+		.attr("y", 10)
+		.attr("text-anchor", "end")
+		.style("text-transform", "capitalize")
+		.text(continent);
+
+});
+
 d3.json("data/data.json").then(function (data) {
 	console.log(data);
 
 	// CLEAN DATA
-	const formattedData = data.map(function (year) {
+	formattedData = data.map(function (year) {
 		return year["countries"].filter(function (country) {
 			var dataExists = (country.income && country.life_exp);
 			return dataExists
@@ -84,23 +126,63 @@ d3.json("data/data.json").then(function (data) {
 		})
 	});
 
-	// RUN THE CODE EVERY 0.1 SEC
-	d3.interval(function () {
-
-		// AT THE END OF OUR DATA, LOOP BACK
-		time = (time < 214) ? time + 1 : 0
-		update(formattedData[time]);
-	}, 100);
-
 	// FIRST RUN OF THE VISUALIZATION
 	update(formattedData[0]);
 
 })
 
+$("#play-button")
+	.on("click", function () {
+		var button = $(this);
+		if (button.text() == "Play") {
+			button.text("Pause");
+			interval = setInterval(step, 100);
+		} else {
+			button.text("Play");
+			clearInterval(interval);
+		}
+	})
+
+$("#reset-button")
+	.on("click", function () {
+		time = 0;
+		update(formattedData[0]);
+	})
+
+$("#continent-select")
+	.on("change", function () {
+		update(formattedData[time]);
+	})
+
+$("#date-slider").slider({
+	max: 2014,
+	min: 1800,
+	step: 1,
+	slide: function (event, ui) {
+		time = ui.value - 1800;
+		update(formattedData[time]);
+	}
+})
+
+function step() {
+	// AT THE END OF OUR DATA, LOOP BACK
+	time = (time < 214) ? time + 1 : 0
+	update(formattedData[time]);
+}
+
 function update(data) {
 	// STANDARD TRANSITION TIME FOR THE VISUALIZATION
 	var t = d3.transition()
 		.duration(100);
+
+	var continent = $("#continent-select").val();
+
+	var data = data.filter(function (d) {
+		if (continent == "all") { return true; }
+		else {
+			return d.continent == continent;
+		}
+	})
 
 	// JOIN NEW DATA WITH OLD ELEMENTS.
 	var circles = g.selectAll("circle").data(data, function (d) {
@@ -117,6 +199,8 @@ function update(data) {
 		.append("circle")
 		.attr("class", "enter")
 		.attr("fill", function (d) { return continentColor(d.continent); })
+		.on("mouseover", tip.show)
+		.on("mouseout", tip.hide)
 		.merge(circles)
 		.transition(t)
 		.attr("cy", function (d) { return y(d.life_exp); })
@@ -125,4 +209,8 @@ function update(data) {
 
 	// UPDATE THE TIME LABEL
 	timeLabel.text(+(time + 1800))
+
+	$("#year")[0].innerHTML = +(time + 1800)
+
+	$("#date-slider").slider("value", +(time + 1800))
 }
